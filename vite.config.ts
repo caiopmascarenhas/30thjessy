b1d32ab0d494f945e3e34a802bf3f3a7ac292a6b
Caio Pereira Mascarenhas <caio.pereira1998@hotmail.com>
2026-08-09 20:20:41 -0300

    feat: new project

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/30th-jessy/',
+  base: '/30thjessy/',
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./src', import.meta.url)),
